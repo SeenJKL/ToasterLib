27da5c11cbba6d75fba2d3a980b4f7678e479e50
SeenJKL <jakarinsn@gmail.com>
2024-08-28 15:08:01 +0700

    ToastMessage Module

---
+++ app/src/main/java/seen/tutorials/sampleapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package seen.tutorials.sampleapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import seen.tutorials.toastermessage.ToasterMessage
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ToasterMessage.s(this, "Hello World")
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
